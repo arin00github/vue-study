c61dd515d86c7b2895fd0737a304661f3a07b826
otter0104 <otter0104@innodep.com>
2023-10-06 13:28:57 +0900

    💡 Update: form 양식 작업

--- src/components/RegisterSection.vue
+++ src/components/RegisterSection.vue
@@ -1,7 +1,50 @@
 t
 <template>
   <div>
-    <h3>Register Section</h3>
+    <form>
+      <div class="form-control">
+        <label for="form-name">이름</label>
+        <input
+          type="text"
+          id="form-name"
+          name="name"
+          placeholder="이름 입력"
+          v-model="form.name"
+        />
+      </div>
+      <div class="radio-group form-control">
+        <div class="radio-set">
+          <input type="radio" id="10age" value="10" v-model="form.age" />
+          <label for="10age">10age</label>
+        </div>
+        <div class="radio-set">
+          <input type="radio" id="20age" value="20" v-model="form.age" />
+          <label for="10age">20age</label>
+        </div>
+        <div class="radio-set">
+          <input type="radio" id="30age" value="30" v-model="form.age" />
+          <label for="10age">30age</label>
+        </div>
+      </div>
+      <div class="form-control">
+        <select
+          name="auth_means"
+          id="form-auth_means"
+          v-model="form.auth_means"
+          placeholder="인증수단 선택"
+        >
+          <option value="naver">네이버</option>
+          <option value="kakao">카카오</option>
+        </select>
+      </div>
+      <div class="form-control">
+        <input type="checkbox" id="agree-check" v-model="form.agree" />
+        <label for="agree-check">개인수집정보 동의</label>
+      </div>
+      <div>
+        <button @click="handleSubmit">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -9,5 +52,38 @@ t
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "RegisterSection",
+  data() {
+    return {
+      form: {
+        name: "",
+        auth_means: undefined,
+        agree: false,
+        age: undefined,
+      },
+    };
+  },
+  methods: {
+    handleSubmit(e: any) {
+      e.preventDefault();
+      const newData = this.form;
+    },
+  },
 });
 </script>
+
+<style>
+.form-control {
+  padding-bottom: 14px;
+}
+.form-control:first-child {
+  padding-top: 30px;
+}
+.radio-group {
+  display: flex;
+  justify-content: center;
+}
+
+.radio-set {
+  margin-left: 12px;
+}
+</style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Register View Page</h1>
+    <RegisterSection />
   </div>
 </template>
 
